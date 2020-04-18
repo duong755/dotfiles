2aa5362c4a8c1c07f9423f80ba8f08b9b72f2a82
duong755 <ngo.quang.duong.1100@gmail.com>
2020-04-18 23:50:55 +0700

    Add zsh install script

--- install.sh
+++ install.sh
@@ -21,9 +21,17 @@ sudo apt install -y curl
 sudo apt install -y tmux
 sudo apt install -y neovim
 sudo apt install -y vim
+sudo apt install -y zsh
 
 cp "${CURRENT_DIRECTORY_PATH}/default.tmux.conf" ~/.tmux.conf
 
 sh "${CURRENT_DIRECTORY_PATH}/python-install.sh"
 sh "${CURRENT_DIRECTORY_PATH}/node-install.sh"
 sh "${CURRENT_DIRECTORY_PATH}/plug-install.sh"
+
+echo "Installing zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ ! -d "$ZSH/custom/plugins/zsh-autosuggestions" ]; then
+  echo "Installing zsh-autosuggestions"
+  git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH/custom/plugins/zsh-autosuggestions
+fi
