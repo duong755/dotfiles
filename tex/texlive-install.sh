a096677e78594196543095cfe78c4398ee094dc8
duong755 <ngo.quang.duong.1100@gmail.com>
2020-11-14 15:52:47 +0700

    config.asy and latexmkrc

--- tex/texlive-install.sh
+++ tex/texlive-install.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+CURRENT_FILE_PATH=$(realpath "$0")
+CURRENT_DIRECTORY_PATH=$(dirname "$CURRENT_FILE_PATH")
+
+echo "Copying asymptote config..."
+if [ ! -d ~/.asy ]; then
+  mkdir -p ~/.asy
+fi
+cp ${CURRENT_DIRECTORY_PATH}/asyconfig.asy ~/.asy/config.asy
+
+echo "Copying latexmkrc..."
+cp ${CURRENT_DIRECTORY_PATH}/latexmkrc ~/.latexmkrc
+
+echo "Downloading texlive installer..."
 wget -O ~/Downloads/install-tl-unx.tar.gz http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 
 mkdir -p ~/Downloads/install-tl-latest
