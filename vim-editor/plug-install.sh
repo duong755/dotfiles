a4e2d489c5a3fe9c8d279b73071ee6cff79272e4
duong755 <ngo.quang.duong.1100@gmail.com>
2021-03-06 17:15:58 +0700

    Update coc_node_path after install vim/neovim; Remove ipfs

--- vim-editor/plug-install.sh
+++ vim-editor/plug-install.sh
@@ -49,6 +49,12 @@ install() {
     all)
       cp "${CURRENT_DIRECTORY_PATH}/init.vim" "${CONFIG}"
       cp "${CURRENT_DIRECTORY_PATH}/coc-settings.json" "${CONFIG_DIR}/coc-settings.json"
+      source ${HOME}/.nvm/nvm.sh
+      NODE_VERSION=$(nvm ls | grep -Po -m 1 '\-\>\s+(v\d{1,2}\.\d{1,2}\.\d{1,2})' | grep -Po 'v\d{1,2}\.\d{1,2}\.\d{1,2}')
+      NODE_PATH=${HOME}/.nvm/versions/node/${NODE_VERSION}/bin/node
+      nvm use $NODE_VERSION
+      nvm alias default $NODE_VERSION
+      sed -i "s;let g:coc_node_path = '.*';let g:coc_node_path = '"$NODE_PATH"';g" ~/.vimrc
       ;;
     minimal)
       cp "${CURRENT_DIRECTORY_PATH}/minimal.vim" "${CONFIG}"
