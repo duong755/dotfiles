2d382ad6cecc24625b6b9240c7660b22286018f0
duong755 <ngo.quang.duong.1100@gmail.com>
2021-05-07 14:50:03 +0700

    :CocUpdate

--- vim-editor/update.sh
+++ vim-editor/update.sh
@@ -4,15 +4,14 @@ update-vim() {
   vim +PlugUpgrade +qall
   vim +PlugUpdate +qall
   vim +PlugClean +qall
-  vim +CocUpdate
 }
 
 update-neovim() {
   nvim +PlugUpgrade +qall
   nvim +PlugUpdate +qall
   nvim +PlugClean +qall
-  nvim +CocUpdate
 }
 
 update-vim
 update-neovim
+vim +CocUpdate
