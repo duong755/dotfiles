4c7f3006f1736562e9b2a001023d81367c1645f4
duong755 <ngo.quang.duong.1100@gmail.com>
2021-05-30 13:03:09 +0700

    Update node and reinstall packages

--- node/update.sh
+++ node/update.sh
@@ -2,15 +2,19 @@
 
 source ~/.nvm/nvm.sh
 
+CURRENT_NODE_VERSION=$(nvm ls | grep -Po -m 1 '\-\>\s+((iojs\-)?v\d+\.\d+\.\d+)' | grep -Po '(iojs\-)?v\d+\.\d+\.\d+')
+
 nvm install --lts --default
 
-NODE_VERSION=$(nvm ls | grep -Po -m 1 '\-\>\s+((iojs\-)?v\d+\.\d+\.\d+)' | grep -Po '(iojs\-)?v\d+\.\d+\.\d+')
+NEXT_NODE_VERSION=$(nvm ls | grep -Po -m 1 '\-\>\s+((iojs\-)?v\d+\.\d+\.\d+)' | grep -Po '(iojs\-)?v\d+\.\d+\.\d+')
 
-NODE_PATH=~/.nvm/versions/node/${NODE_VERSION}/bin/node
+NODE_PATH=~/.nvm/versions/node/${NEXT_NODE_VERSION}/bin/node
 
-nvm use $NODE_VERSION
+nvm use $NEXT_NODE_VERSION
 
-nvm alias default $NODE_VERSION
+nvm alias default $NEXT_NODE_VERSION
+
+nvm reinstall-packages ${CURRENT_NODE_VERSION}
 
 sed -i "s;let g:coc_node_path = '.*';let g:coc_node_path = '"$NODE_PATH"';g" ~/.vimrc
 sed -i "s;let g:coc_node_path = '.*';let g:coc_node_path = '"$NODE_PATH"';g" ~/.config/nvim/init.vim
