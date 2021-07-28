02210588d8ea1df75a6620776159ff8c17aa0d46
duong755 <ngo.quang.duong.1100@gmail.com>
2021-07-28 17:07:58 +0700

    Install coc-tailwindcss; Change bind key for tmux

--- vim/plug-install.sh
+++ vim/plug-install.sh
@@ -83,11 +83,11 @@ install() {
   case $1 in
     vim)
       vim +PlugInstall +qall
-      vim +CocInstall coc-html coc-css coc-emmet coc-pairs coc-highlight coc-eslint coc-tsserver coc-json coc-angular coc-git +qall
+      vim +CocInstall coc-html coc-css coc-tailwindcss coc-emmet coc-pairs coc-highlight coc-eslint coc-tsserver coc-json coc-angular coc-git +qall
       ;;
     nvim | neovim)
       nvim +PlugInstall +qall
-      nvim +CocInstall coc-html coc-css coc-emmet coc-pairs coc-highlight coc-eslint coc-tsserver coc-json coc-angular coc-git +qall
+      nvim +CocInstall coc-html coc-css coc-tailwindcss coc-emmet coc-pairs coc-highlight coc-eslint coc-tsserver coc-json coc-angular coc-git +qall
       ;;
     *)
       echo "Select one of these editors: vim, nvim, neovim."
