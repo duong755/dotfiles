36a33b0b7b7b7ac1587dff2d448ba4edb5a16bb6
duong755 <ngo.quang.duong.1100@gmail.com>
2021-08-08 23:41:42 -0700

    Move update-tlmgr-latest.sh

--- tex/tlmgr-update.sh
+++ tex/tlmgr-update.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-wget -O /root/update-tlmgr-latest.sh http://mirror.ctan.org/systems/texlive/tlnet/update-tlmgr-latest.sh
-chmod +x /root/update-tlmgr-latest.sh
-sudo env PATH="${PATH}" /root/update-tlmgr-latest.sh
+if [ $UID != "0" ];then
+  echo "Require root user"
+  exit
+fi
+
+wget -O /usr/local/texlive/2021/bin/x86_64-linux/update-tlmgr-latest.sh http://mirror.ctan.org/systems/texlive/tlnet/update-tlmgr-latest.sh
+chmod +x /usr/local/texlive/2021/bin/x86_64-linux/update-tlmgr-latest.sh
+env PATH="${PATH}" /usr/local/texlive/2021/bin/x86_64-linux/update-tlmgr-latest.sh
