d5ce99fd5d193b07e7313885d26f4bf0f6bd2802
duong755 <ngo.quang.duong.1100@gmail.com>
2022-09-01 18:14:11 +0700

    Remove repetitive code in docker-compose.sh

--- docker/docker-compose.sh
+++ docker/docker-compose.sh
@@ -3,6 +3,8 @@
 API_ENDPOINT_LIST_RELEASES=https://api.github.com/repos/docker/compose/releases
 CLI_PLUGINS_DIR=/usr/libexec/docker/cli-plugins
 GLOBAL_BINARY=/usr/bin/docker-compose
+ARCH=$(uname -m)
+RELEASE_NAME="docker-compose-linux-$ARCH"
 
 function all_versions() {
   curl -s "$API_ENDPOINT_LIST_RELEASES" | jq '.[] | { tag_name: .tag_name } | .tag_name'
@@ -14,8 +16,6 @@ function get_latest_version() {
     exit
   fi
 
-  ARCH=$(uname -m)
-  RELEASE_NAME="docker-compose-linux-$ARCH"
   DOWNLOAD_URL=$(wget -qO- "$API_ENDPOINT_LIST_RELEASES" | jq -r --arg NAME "$RELEASE_NAME" '.[0] | { asset: .assets[] | select(.name==$NAME) } | .asset.browser_download_url')
   OUTPUT="$CLI_PLUGINS_DIR/$RELEASE_NAME"
 
@@ -30,8 +30,6 @@ function get_version() {
     exit
   fi
 
-  ARCH=$(uname -m)
-  RELEASE_NAME="docker-compose-linux-$ARCH"
   DOWNLOAD_URL=$(wget -qO- "$API_ENDPOINT_LIST_RELEASES" | jq -r --arg NAME "$1" '.[0] | { asset: .assets[] | select(.name==$NAME) } | .asset.browser_download_url')
   OUTPUT="$CLI_PLUGINS_DIR/$RELEASE_NAME"
 
