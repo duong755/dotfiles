aeb71fe9d37d8ec363ff36b620ba99a1467fb02f
duong755 <ngo.quang.duong.1100@gmail.com>
2022-10-01 22:51:52 +0700

    config git

---
+++ os/gitconfig.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git config --global core.editor vim
+git config --global core.autocrlf=input
+git config --global core.eol=lf
+git config --global diff.tool=vimdiff
+git config --global user.name=duong755
+git config --global user.email=ngo.quang.duong.1100@gmail.com
+git config --global merge.tool=vimdiff
+git config --global merge.ff=true
+git config --global init.defaultbranch=master
+git config --global push.autosetupremote=true
+git config --global gpg.program=gpg2
